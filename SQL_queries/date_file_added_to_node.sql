3363905c27adaadd9a8f67f690290ab2d8e4bdc6
csoderberg <csoderbe@gmail.com>
2019-09-17 13:56:45 -0400

    only get ids for files on non-deleted projects

--- SQL_queries/date_file_added_to_node.sql
+++ SQL_queries/date_file_added_to_node.sql
@@ -1,10 +1,13 @@
 
 /* get WB id for each file uploaded to osf_storage */
 WITH view_links AS (SELECT json_extract_path_text(params::json, 'urls', 'view') AS view_link, id, node_id, original_node_id, date
-						FROM osf_nodelog
-						WHERE (action = 'osf_storage_file_added' OR action = 'file_added') AND 
-							node_id = original_node_id /* filtering out forks */
-						LIMIT 10000),
+						FROM osf_abstractnode
+						LEFT JOIN (SELECT *
+									FROM osf_nodelog
+									WHERE action = 'osf_storage_file_added' OR action = 'file_added'
+									LIMIT 10000) AS logs
+						ON osf_abstractnode.id = logs.node_id
+						WHERE is_deleted IS FALSE AND title NOT LIKE 'Bookmarks' AND type = 'osf.node'),
 	wb_ids AS (SELECT id, view_link, reverse(split_part(reverse(view_link), '/', 2)) AS wb_id, node_id, date
 					FROM view_links
 					WHERE node_id = original_node_id)

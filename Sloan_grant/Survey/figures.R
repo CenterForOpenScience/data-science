d23e55436327e566552e466e411b62a7e98e58ad
csoderberg <csoderbe@gmail.com>
2019-10-03 16:00:20 -0400

    add figure for overall credibility of icons

--- Sloan_grant/Survey/figures.R
+++ Sloan_grant/Survey/figures.R
@@ -30,5 +30,40 @@ survey_data <- read_csv(here::here('cleaned_data.csv'), col_types = cols(.defaul
                                                                          DistributionChannel = col_character()))
 
 ## Overall icons importance
+preprint_cred <- survey_data %>%
+  select(preprint_cred1_1:preprint_cred5_3)
+
+choices  <- c('Not at all important', 'Slightly important', 'Moderately important', 'Very important', 'Extremely important')
+
+colnames(preprint_cred) <- c(preprint_cred1_1 = "Author's previous work",
+                             preprint_cred1_2 = "Author's institution",
+                             preprint_cred1_3 = "Professional identity links",
+                             preprint_cred1_4 = "COI disclosures",
+                             preprint_cred1_5 = "Author's level of open scholarship",
+                             preprint_cred2_1 = "Funders of research",
+                             preprint_cred2_2 = "Preprint submitted to a journal",
+                             preprint_cred2_3 = "Usage metrics",
+                             preprint_cred2_4 = "Citations of preprints",
+                             preprintcred3_1 = "Anonymous comments",
+                             preprintcred3_2 = "Identified comments",
+                             preprintcred3_3 = "Simplified endorsements",
+                             preprint_cred4_1 = "Link to study data",
+                             preprint_cred4_2 = "Link to study analysis scripts",
+                             preprint_cred4_3 = "Link to materials",
+                             preprint_cred4_4 = "Link to pre-reg",
+                             preprint_cred5_1 = "Info about indep groups accessing linked info",
+                             preprint_cred5_2 = "Info about indep group reproductions",
+                             preprint_cred5_3 = "Info about indep robustness checks")
+
+preprint_cred <- preprint_cred %>%
+  mutate_all(factor, levels=1:5, labels=choices, ordered=TRUE)
+
+cred_preprints<- expression(atop("When assessing the credibility of a preprint", paste("how important would it be to have each of the following pieces of information?")))
+pdf("icon_cred.pdf", width=12.5, height=10)
+plot(likert(as.data.frame(preprint_cred)), ordered=T, text.size = 4) + 
+  ggtitle(cred_preprints)+
+  theme(plot.title = element_text(hjust = 0.5, size = 16), axis.text = element_text(size = 12), legend.title = element_blank(), legend.text=element_text(size=10))
+dev.off()
+
 
 ## Overall service credibilitys

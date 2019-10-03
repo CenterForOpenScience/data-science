ee4267f75d7f36a0b5327570d4f35c93301bed62
csoderberg <csoderbe@gmail.com>
2019-10-03 16:11:27 -0400

    add figure for credibility of service

--- Sloan_grant/Survey/figures.R
+++ Sloan_grant/Survey/figures.R
@@ -67,3 +67,40 @@ dev.off()
 
 
 ## Overall service credibilitys
+
+service_cred <- survey_data %>%
+  select(services_cred1_1:service_credible4_5)
+
+choices <- c('Decrease a lot', "Moderately decrease", "Slightly decrease", "Neither decrease nor increase", "Slightly increase", "Moderately increase", "Increase a lot")
+
+colnames(service_cred) <- c(services_cred1_1 = "Service moderators that screen for spam and non-scholarly content", 
+                            services_cred1_2 = "Scholars in my field are involved in the operation of the service (e.g., via an advisory board)",
+                            services_cred1_3 = "Service has clear policies about misconduct and plagiarism and a mechanism to flag content that may breach these policies", 
+                            services_cred1_4 = "Service assesses the reproducibility of reported findings and indicates the results of their assessment on each preprint",
+                            service_cred2_1 = "The software running the service is open source (e.g. openly licensed)", 
+                            service_cred2_2 = "The service’s business model is transparent, stable and sustainable", 
+                            service_cred2_3 = "Service has a mechanism for long-term preservation of content",
+                            service_cred2_4 = "Service is free for preprint submitters and readers", 
+                            service_cred3_1 = "Service enables preprints to be submitted to journals directly from the service site", 
+                            service_cred3_2 = "Service clearly indicates which content, if any, has been published in a peer-reviewed journal",
+                            service_cred3_3 = "Service allows endorsements of preprints by independent users (i.e. non-authors)", 
+                            service_cred3_4 = "Service is indexed by search and discovery services (e.g., Google Scholar, Crossref)", 
+                            service_cred3_5 = "Service is popular in my discipline", 
+                            service_credible4_1 = "Ability to assign a DOI to the preprint", 
+                            service_credible4_2 = "Service enables anonymous posting of preprints", 
+                            service_credible4_3 = "Service allows authors to remove their preprints for any reason",
+                            service_credible4_4 = "Once posted, the service controls remove/withdrawal of preprints", 
+                            service_credible4_5 = "Service enables submission of new versions of the preprint")
+
+
+service_cred <- service_cred %>%
+  mutate_all(factor, levels=-3:3, labels=choices, ordered=TRUE)
+
+service_preprints<- expression(atop("To what extent would having each of the following features ", paste("decrease or increase the credibility of a preprint service?")))
+pdf("service_cred.pdf", width=12.5, height=10)
+plot(likert(as.data.frame(service_cred)), ordered=T) + ggtitle(service_preprints)+
+  theme(plot.title = element_text(hjust = 0.5), legend.title = element_blank())
+dev.off()
+
+
+

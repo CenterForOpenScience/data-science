6757437aa7a45f28c61ad54a3508b5927eed1230
csoderberg <csoderbe@gmail.com>
2019-10-22 11:30:49 -0400

    load in cleaned data

---
+++ Sloan_grant/Survey/Survey_analyses.R
@@ -0,0 +1,30 @@
+##required libraries
+library(osfr)
+library(tidyverse)
+library(here)
+
+## reading in data
+osf_retrieve_file("https://osf.io/86upq/") %>% 
+  osf_download(overwrite = T)
+
+survey_data <- read_csv(here::here('cleaned_data.csv'), col_types = cols(.default = col_number(),
+                                                                         StartDate = col_datetime(format = '%m/%d/%y %H:%M'),
+                                                                         EndDate = col_datetime(format = '%m/%d/%y %H:%M'),
+                                                                         ResponseId = col_character(),
+                                                                         position_7_TEXT = col_character(), 
+                                                                         familiar = col_factor(),
+                                                                         preprints_submitted = col_factor(),
+                                                                         preprints_used = col_factor(),
+                                                                         position = col_factor(),
+                                                                         acad_career_stage = col_factor(),
+                                                                         country = col_factor(),
+                                                                         discipline = col_character(),
+                                                                         discipline_specific = col_character(),
+                                                                         discipline_other = col_character(),
+                                                                         bepress_tier1 = col_character(),
+                                                                         bepress_tier2 = col_character(),
+                                                                         bepress_tier3 = col_character(),
+                                                                         discipline_collapsed = col_factor(),
+                                                                         how_heard = col_character(),
+                                                                         hdi_level = col_factor(),
+                                                                         age = col_character()))

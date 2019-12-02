57733fefff08279fec069874181d995869cbd7f2
csoderberg <csoderbe@gmail.com>
2019-12-02 16:20:54 -0500

    add keen data access adn cleaning through googledrive

--- Monthly_Reports/Registrations/data_processing.R
+++ Monthly_Reports/Registrations/data_processing.R
@@ -1,6 +1,22 @@
 library(tidyverse)
 library(lubridate)
 library(osfr)
+library(googlesheets4)
+library(here)
+
+
+## create monthly numbers for total registrations based on keen daily data
+read_sheet('https://docs.google.com/spreadsheets/d/1ti6iEgjvr-hXyMT5NwCNfAg-PJaczrMUX9sr6Cj6_kM/', 
+           col_types = '??ii') %>%
+    select(keen.timestamp, registered_projects.total) %>%
+    mutate(keen.timestamp = ymd_hms(keen.timestamp),
+           year_month  = format(keen.timestamp, "%Y-%m")) %>%
+    group_by(year_month) %>%
+    filter(keen.timestamp == max(keen.timestamp)) %>%
+    ungroup() %>%
+    mutate(monthly_diff = registered_projects.total - lag(registered_projects.total)) %>%
+    write_csv(here::here('Monthly_Reports/Registrations/', 'monthly_total_regs.csv'))
+                    
 
 last_month <- floor_date(Sys.Date() - months(1), "month") %>%
                   str_sub(1, 7)

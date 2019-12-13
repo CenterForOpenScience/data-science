5095a38e43042db3ae780bfb4382ae4272d54784
csoderberg <csoderbe@gmail.com>
2019-12-13 13:50:36 -0500

    save query to get number of users by email domain

---
+++ SQL_queries/email_metrics.sql
@@ -0,0 +1,15 @@
+/* count of emails by institution */
+
+/* get a list of all the institutions registered users have email accounts with */
+WITH emails AS (SELECT SPLIT_PART(address, '@', 2) AS institution, user_id, address, is_registered, is_active 
+	FROM osf_email
+	LEFT JOIN osf_osfuser
+	ON osf_email.user_id = osf_osfuser.id
+	WHERE is_registered IS TRUE)
+
+/* count up the number of users by institution*/	
+SELECT COUNT(user_id), institution
+	/* remove duplicate user/institution rows for schools like UVA that allow email aliases*/
+	FROM (SELECT DISTINCT user_id, institution
+			FROM emails) AS deduplicate
+	GROUP BY institution;

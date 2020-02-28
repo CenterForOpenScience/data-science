ced1ed848645d4d4eccc64861e5e08a4742e7daa
csoderberg <csoderbe@gmail.com>
2020-02-28 11:22:14 -0500

    add query for new_signup counts

--- SQL_queries/user_generation_paths.sql
+++ SQL_queries/user_generation_paths.sql
@@ -2,12 +2,19 @@
 
 
 /* intital query to get info about users with system tags, dates, and SSO */
-SELECT username, is_registered, is_invited, date_registered, date_confirmed, is_active, deleted, spam_status, osf_tag.name, institution_id
-	FROM osf_osfuser
-	LEFT JOIN osf_osfuser_tags
-	ON osf_osfuser.id = osf_osfuser_tags.osfuser_id
-	LEFT JOIN osf_tag
-	ON osf_osfuser_tags.tag_id = osf_tag.id
-	LEFT JOIN osf_osfuser_affiliated_institutions
-	ON osf_osfuser.id = osf_osfuser_affiliated_institutions.osfuser_id
-	WHERE osf_tag.system IS TRUE;
+WITH user_tag_info AS (SELECT osf_osfuser.id AS user_id, username, is_registered, is_invited, date_registered, date_confirmed, date_disabled, is_active, deleted, spam_status, osf_tag.name, institution_id
+						FROM osf_osfuser
+						LEFT JOIN osf_osfuser_tags
+						ON osf_osfuser.id = osf_osfuser_tags.osfuser_id
+						LEFT JOIN osf_tag
+						ON osf_osfuser_tags.tag_id = osf_tag.id
+						LEFT JOIN osf_osfuser_affiliated_institutions
+						ON osf_osfuser.id = osf_osfuser_affiliated_institutions.osfuser_id
+						WHERE osf_tag.system IS TRUE AND (osf_tag.name NOT LIKE '%spam' AND osf_tag.name != 'high_upload_limit' AND osf_tag.name != 'ham_confirmed'))
+
+SELECT COUNT(user_id) AS new_signups, name
+	FROM user_tag_info
+	WHERE is_registered IS TRUE AND is_invited IS FALSE
+	GROUP BY name;
+	
+

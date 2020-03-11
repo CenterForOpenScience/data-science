72a9d414a3928aed64cbf67552801ca82c14c393
csoderberg <csoderbe@gmail.com>
2020-03-11 16:42:34 -0400

    add query for affiliated pp count

--- SQL_queries/preprint_info query.sql
+++ SQL_queries/preprint_info query.sql
@@ -123,3 +123,36 @@ SELECT pp.id, pp.created, pp.modified, date_published, license_id, pp.node_id, p
 	FULL OUTER JOIN supp_node_files
 	ON pp.node_id = supp_node_files.supp_node_id
 	WHERE pp.is_published = 'TRUE' AND (pp.machine_state = 'pending' OR pp.machine_state = 'accepted') AND osf_guid.content_type_id = 47 AND pp.is_public = 'TRUE' AND primary_file_id IS NOT NULL AND pp.deleted IS NULL;
+
+
+## counting, for each OSF4I, how many preprints have contritbutors who have SSO, and how many users with SSO are contributors on preprints
+WITH users_instit_pps AS (SELECT osfuser_id, institution_id, preprint_id, osf_institution._id AS OSF4I, osf_institution.name
+							FROM osf_osfuser_affiliated_institutions
+							LEFT JOIN osf_preprintcontributor
+							ON osf_osfuser_affiliated_institutions.osfuser_id = osf_preprintcontributor.user_id
+							LEFT JOIN osf_preprint
+							ON osf_preprintcontributor.preprint_id = osf_preprint.id
+							LEFT JOIN osf_institution
+							ON osf_osfuser_affiliated_institutions.institution_id = osf_institution.id
+							LEFT JOIN osf_osfuser
+							ON osf_osfuser_affiliated_institutions.osfuser_id = osf_osfuser.id
+							WHERE is_registered = TRUE AND 
+									date_confirmed IS NOT NULL AND 
+									date_disabled IS NULL AND 
+									is_published IS TRUE AND 
+									(machine_state = 'accepted' OR machine_state = 'pending') AND 
+									ever_public = TRUE AND 
+									date_withdrawn IS NULL AND 
+									is_public IS TRUE AND 
+									osf_preprint.deleted IS NULL)
+
+SELECT distinct on (users_instit_pps.name) users_instit_pps.name, num_users, num_preprints
+	FROM users_instit_pps
+	LEFT JOIN (SELECT name, COUNT(DISTINCT osfuser_id) AS num_users 
+					FROM users_instit_pps 
+					GROUP BY name) AS n_users
+	ON users_instit_pps.name = n_users.name
+	LEFT JOIN (SELECT name, COUNT(DISTINCT preprint_id) AS num_preprints
+					FROM users_instit_pps 
+					GROUP BY name) AS n_preprints
+	ON users_instit_pps.name = n_preprints.name;

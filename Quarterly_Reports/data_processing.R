bfb9b15c7ccda490633a7af3bfc9beedf475f5a7
csoderberg <csoderbe@gmail.com>
2020-03-19 11:37:48 -0400

    create complete monthly history and cumsums

--- Quarterly_Reports/data_processing.R
+++ Quarterly_Reports/data_processing.R
@@ -33,3 +33,27 @@ hist_cat_data <- hist_data %>%
                     summarize(new_signups = sum(new_signups), new_claims = sum(new_claims), new_sources = sum(new_sources), 
                               sso_newsignups = sum(sso_newsignups), sso_newclaims = sum(sso_newclaims))
 
+# get monthly data for all product types for all months (so add in 0s)
+
+date <- as_tibble(seq(as.Date('2012-09-01'), floor_date(as.Date(Sys.time()), 'month'), by = 'month')) %>%
+          rename(date = value) %>%
+          mutate(date = as_datetime(date),
+                 date = floor_date(date, 'month'),
+                 placeholder = 1)
+        
+products <- as_tibble(c('osf4m', 'preprints', 'registries', 'osf')) %>%
+              rename(product_type = value) %>%
+              mutate(placeholder = 1)
+
+full_hist_cat_date <- full_join(date, products, by = 'placeholder') %>%
+                          left_join(hist_cat_data, by = c("date" = "month", 'product_type' = 'product_type')) %>%
+                          select(-placeholder) %>%
+                          replace(., is.na(.), 0) %>%
+                          group_by(product_type) %>%
+                          mutate(cum_new_signups = cumsum(new_signups),  #create columns for running totals
+                                 cum_new_claims = cumsum(new_claims),
+                                 cum_new_sources = cumsum(new_sources),
+                                 cum_sso_newsignups = cumsum(sso_newsignups),
+                                 cum_sso_newclaims = cumsum(sso_newclaims))
+
+

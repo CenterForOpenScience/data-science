bbd8b7bd84080c0c43fa66a6cb6c3cc8071a8d68
csoderberg <csoderbe@gmail.com>
2020-03-22 16:54:27 -0400

    finish suppnode categorizatoin

--- SQL_queries/downloads_by_product.sql
+++ SQL_queries/downloads_by_product.sql
@@ -12,10 +12,11 @@ WITH
 										SELECT cl.id, cl._id, cl.is_node_link, rl.child_id, rl.parent_id, suppnode_id
 											FROM osf_noderelation rl
 											INNER JOIN children_nodes cl
-											ON cl.child_id = rl.parent_id),
+											ON cl.child_id = rl.parent_id
+											WHERE rl.is_node_link IS FALSE),
 
 	 /* dedpublicate resulting relations since single suppnode can be connected to mulitple preprints */
-	 suppnode_relations AS (SELECT DISTINCT ON (child_id) child_id, suppnode_id, id
+	 suppnode_relations AS (SELECT DISTINCT ON (child_id) child_id, suppnode_id, parent_id, id
 								FROM children_nodes),
 	 daily_format AS (SELECT *, json_object_keys(date::json) AS download_date, json_each_text(date::json) AS daily_downloads
 						FROM osf_pagecounter
@@ -32,7 +33,8 @@ WITH
 							WHERE target_content_type_id = 47
 							GROUP BY date_trunc('month', download_date)),
  	monthly_file_downloads AS (SELECT daily_downloads.id, target_object_id, osf_abstractnode.id, download_date, total, type, spam_status, name,
- 									CASE WHEN institution_id IS NOT NULL THEN 1 ELSE 0 END as inst_affil
+ 									CASE WHEN institution_id IS NOT NULL THEN 1 ELSE 0 END as inst_affil,
+ 									CASE WHEN sr_child.suppnode_id IS NOT NULL OR sr_parent.suppnode_id IS NOT NULL THEN 1 ELSE 0 END as supp_node
 								 FROM daily_downloads
 								 LEFT JOIN osf_abstractnode
 								 ON daily_downloads.target_object_id = osf_abstractnode.id
@@ -41,16 +43,20 @@ WITH
 								 				LEFT JOIN osf_tag
 								 				ON osf_abstractnode_tags.tag_id = osf_tag.id
 								 				WHERE name = 'osf4m') AS project_tags
-								 ON osf_abstractnode.id = project_tags.abstractnode_id,
+								 ON osf_abstractnode.id = project_tags.abstractnode_id
 
 								 /* nodes can be affiliated with multiple institutions, so need to deduplicate before joining in to keep 1 row per node */ 
 								 LEFT JOIN (SELECT DISTINCT ON (abstractnode_id) abstractnode_id, institution_id
 								 				FROM osf_abstractnode_affiliated_institutions) AS deduped_inst_nodes
 								 ON osf_abstractnode.id = deduped_inst_nodes.abstractnode_id
 
-								 /* nodes can be supplements for multiple pps, so need to deduplicate */
-								 LEFT JOIN (SELECT DISTINCT ON (node_id) node_id, id
-								 				FROM osf_preprint)
+								 /* join in parents and children suppnodes seperately to categorize both */
+								 LEFT JOIN suppnode_relations sr_child
+								 ON osf_abstractnode.id = sr_child.child_id
+								 LEFT JOIN (SELECT DISTINCT ON (parent_id) parent_id, suppnode_id, child_id
+				 								FROM suppnode_relations) AS sr_parent
+								 ON osf_abstractnode.id = sr_parent.parent_id
+
 								 WHERE target_content_type_id = 30)
 
 

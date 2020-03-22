575b91feb4b5b42f41121b8bc8b4d21f5e690fc1
csoderberg <csoderbe@gmail.com>
2020-03-22 14:53:00 -0400

    initial query to get 1 row per date

--- SQL_queries/downloads_by_product.sql
+++ SQL_queries/downloads_by_product.sql
@@ -1,11 +1,17 @@
 /* downloads by product type backfill query*/
 
+WITH daily_format AS (SELECT *, json_object_keys(date::json) AS download_date, json_each_text(date::json) AS daily_downloads
+						FROM osf_pagecounter
+						WHERE action = "download"
+						LIMIT 100)
+
 SELECT osf_pagecounter.id, date, osf_pagecounter.modified, file_id, resource_id, version, 
 		target_content_type_id, target_object_id
 	FROM osf_pagecounter
 	LEFT JOIN osf_basefilenode
 	ON osf_pagecounter.file_id = osf_basefilenode.id
 	WHERE action = 'download'
+	LIMIT 100
 
 
 /* downloads by product type per quater */

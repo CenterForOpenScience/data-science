f1c06bac4c60efce74a96087e78efe3c02c2f172
csoderberg <csoderbe@gmail.com>
2020-03-22 15:39:31 -0400

    identify affiliated nodes

--- SQL_queries/downloads_by_product.sql
+++ SQL_queries/downloads_by_product.sql
@@ -14,7 +14,8 @@ WITH daily_format AS (SELECT *, json_object_keys(date::json) AS download_date, j
 							FROM daily_downloads
 							WHERE target_content_type_id = 47
 							GROUP BY date_trunc('month', download_date)),
- 	monthly_file_downloads AS (SELECT daily_downloads.id, target_object_id, osf_abstractnode.id, download_date, total, type, spam_status, name
+ 	monthly_file_downloads AS (SELECT daily_downloads.id, target_object_id, osf_abstractnode.id, download_date, total, type, spam_status, name,
+ 									CASE WHEN institution_id IS NOT NULL THEN 1 ELSE 0 END as inst_affil
 								 FROM daily_downloads
 								 LEFT JOIN osf_abstractnode
 								 ON daily_downloads.target_object_id = osf_abstractnode.id
@@ -23,7 +24,12 @@ WITH daily_format AS (SELECT *, json_object_keys(date::json) AS download_date, j
 								 				LEFT JOIN osf_tag
 								 				ON osf_abstractnode_tags.tag_id = osf_tag.id
 								 				WHERE name = 'osf4m') AS project_tags
-								 ON osf_abstractnode.id = project_tags.abstractnode_id
+								 ON osf_abstractnode.id = project_tags.abstractnode_id,
+
+								 /* nodes can be affiliated with multiple institutions, so need to deduplicate before joining in to keep 1 row per node */ 
+								 LEFT JOIN (SELECT DISTINCT ON (abstractnode_id) abstractnode_id, institution_id
+								 				FROM osf_abstractnode_affiliated_institutions) AS deduped_inst_nodes
+								 ON osf_abstractnode.id = deduped_inst_nodes.abstractnode_id
 								 WHERE target_content_type_id = 30)
 
 

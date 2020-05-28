c21e9eb148c57cfc3c594a5a6a8ec3a954e878ee
csoderberg <csoderbe@gmail.com>
2020-05-28 18:30:40 -0400

    add query for storage on EGAP

--- SQL_queries/storage_queries.sql
+++ SQL_queries/storage_queries.sql
@@ -29,3 +29,20 @@ SELECT osf_collection.title,
 
 
 /* number of different object types and storage by branded registry (EGAP to start) */
+SELECT COUNT(DISTINCT root_id) AS num_toplevel_reg,
+	   COUNT(DISTINCT osf_abstractnode.id) AS num_reg_nodes,
+	   COUNT(DISTINCT osf_files.id) AS num_files,
+	   COUNT(DISTINCT osf_fileversion.id) AS num_versions,
+	   SUM(size) AS storage
+	FROM osf_abstractnode_registered_schema
+	LEFT JOIN osf_abstractnode
+	ON osf_abstractnode_registered_schema.abstractnode_id = osf_abstractnode.id
+	LEFT JOIN (SELECT *
+				 FROM osf_basefilenode
+				 WHERE type = 'osf.osfstoragefile') as osf_files
+	ON osf_abstractnode.id = osf_files.target_object_id
+	LEFT JOIN osf_basefileversionsthrough
+	ON osf_files.id = osf_basefileversionsthrough.basefilenode_id
+	LEFT JOIN osf_fileversion
+	ON osf_basefileversionsthrough.fileversion_id = osf_fileversion.id
+	WHERE registrationschema_id = 26 AND is_deleted IS FALSE

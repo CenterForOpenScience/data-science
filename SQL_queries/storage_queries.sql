973f77f658a5447ffed1c1aa8e1ca7a8b06d8c40
csoderberg <csoderbe@gmail.com>
2020-06-18 13:17:03 -0400

    add views and downloads to collection query

--- SQL_queries/storage_queries.sql
+++ SQL_queries/storage_queries.sql
@@ -1,31 +1,54 @@
-/* number of different object types and storage by collection */
-SELECT osf_collection.title, 
-	   COUNT(DISTINCT project_nodes.root_id) AS num_projects,
-	   COUNT(DISTINCT project_nodes.id) AS num_nodes,
-	   COUNT(DISTINCT osf_files.id) AS num_files,
-	   COUNT(DISTINCT osf_fileversion.id) AS num_versions,
-	   SUM(size) AS storage
-	FROM osf_collectionsubmission
-	LEFT JOIN osf_collection
-	ON osf_collectionsubmission.collection_id = osf_collection.id
-	LEFT JOIN (SELECT *
-				FROM osf_guid
-				WHERE content_type_id = 30) as node_guids
-	ON osf_collectionsubmission.guid_id = node_guids.id
-	LEFT JOIN (SELECT *
-				FROM osf_abstractnode
-				WHERE type = 'osf.node' AND is_deleted IS FALSE AND is_public IS TRUE) as project_nodes
-	ON node_guids.object_id = project_nodes.root_id
-	LEFT JOIN (SELECT *
-				 FROM osf_basefilenode
-				 WHERE type = 'osf.osfstoragefile') as osf_files
-	ON project_nodes.id = osf_files.target_object_id
-	LEFT JOIN osf_basefileversionsthrough
-	ON osf_files.id = osf_basefileversionsthrough.basefilenode_id
-	LEFT JOIN osf_fileversion
-	ON osf_basefileversionsthrough.fileversion_id = osf_fileversion.id
-	WHERE (collection_id = 711617 OR collection_id = 709754)
-	GROUP BY osf_collection.title;
+/* number of different object types, storage, and views/downloads by collection */
+WITH collection_files AS (SELECT osf_collection.title, 
+							   	project_nodes.root_id AS root_id,
+							    project_nodes.id AS node_id,
+							    osf_files.id AS file_id
+							FROM osf_collectionsubmission
+							LEFT JOIN osf_collection
+							ON osf_collectionsubmission.collection_id = osf_collection.id
+							LEFT JOIN (SELECT *
+										FROM osf_guid
+										WHERE content_type_id = 30) as node_guids
+							ON osf_collectionsubmission.guid_id = node_guids.id
+							LEFT JOIN (SELECT *
+										FROM osf_abstractnode
+										WHERE type = 'osf.node' AND is_deleted IS FALSE AND is_public IS TRUE) as project_nodes
+							ON node_guids.object_id = project_nodes.root_id
+							LEFT JOIN (SELECT *
+										 FROM osf_basefilenode
+										 WHERE type = 'osf.osfstoragefile') as osf_files
+							ON project_nodes.id = osf_files.target_object_id
+							WHERE (collection_id = 711617 OR collection_id = 709754)),
+	file_actions AS (SELECT file_id, Min(_id), action, Max(total),
+							CASE WHEN action = 'download' THEN Max(total) ELSE 0 END AS downloads,
+							CASE WHEN action = 'view' THEN Max(total) ELSE 0 END AS views
+							FROM osf_pagecounter
+							WHERE file_id IN (SELECT file_id from collection_files)
+							GROUP BY file_id, action)
+
+SELECT collection_files.title,  
+	   COUNT(DISTINCT root_id) AS num_projects,
+	   COUNT(DISTINCT node_id) AS num_nodes,
+	   COUNT(DISTINCT collection_files.file_id) AS num_files,
+	   SUM(num_versions) AS num_versions,
+	   SUM(storage) AS storage,
+	   SUM(downloads) AS downloads,
+	   SUM(views) AS views
+	FROM collection_files
+	LEFT JOIN (SELECT osf_basefilenode.id, Max(target_object_id) as target_object_id, SUM(size) AS storage, COUNT(DISTINCT osf_fileversion.id) AS num_versions
+					FROM osf_basefilenode
+					LEFT JOIN osf_basefileversionsthrough
+					ON osf_basefilenode.id = osf_basefileversionsthrough.basefilenode_id
+					LEFT JOIN osf_fileversion
+					ON osf_basefileversionsthrough.fileversion_id = osf_fileversion.id
+					WHERE type = 'osf.osfstoragefile' AND osf_basefilenode.id IN (SELECT file_id from collection_files)
+					GROUP BY osf_basefilenode.id) AS file_info
+	ON collection_files.file_id = file_info.id
+	LEFT JOIN (SELECT file_id, SUM(downloads) as downloads, SUM(views) AS views
+				 FROM file_actions
+				 GROUP BY file_id) as actions_file
+	ON collection_files.file_id = actions_file.file_id
+	GROUP BY collection_files.title;
 
 
 /* number of different object types and storage by branded registry (EGAP to start) */

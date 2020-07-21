a1ca2abdf30aff58de21d9653b6bc698c2496272
csoderberg <csoderbe@gmail.com>
2020-07-21 15:21:47 -0400

    update tagging selection

--- SQL_queries/user_generation_paths.sql
+++ SQL_queries/user_generation_paths.sql
@@ -11,7 +11,7 @@ WITH user_tag_info AS (SELECT osf_osfuser.id AS user_id, username, is_registered
 						LEFT JOIN osf_osfuser_affiliated_institutions
 						ON osf_osfuser.id = osf_osfuser_affiliated_institutions.osfuser_id
 						WHERE osf_tag.system IS TRUE AND 
-							(osf_tag.name NOT LIKE '%spam' AND osf_tag.name != 'high_upload_limit' AND osf_tag.name != 'ham_confirmed' AND osf_tag.name NOT LIKE '%metrics' AND osf_tag.name != 'prereg_admin')),
+							(osf_tag.name LIKE 'source%' OR osf_tag.name LIKE 'claimed%')),
 	 new_signups AS (SELECT COUNT(user_id) AS new_signups, 
 	 						COUNT(CASE WHEN institution_id IS NOT NULL THEN 1 END) AS sso_newsignups, 
 	 						date_trunc('month', date_confirmed) as month, name

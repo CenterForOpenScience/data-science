510b78ffef2b6b32147fd9d39ee914fd3f325e1c
csoderberg <csoderbe@gmail.com>
2020-07-22 11:51:52 -0400

    update cumulative totals

--- Quarterly_Reports/data_processing.R
+++ Quarterly_Reports/data_processing.R
@@ -24,27 +24,21 @@ osf_upload()
 #### calculate summary variables for report ####
 
 # get monthly numbers by product type
-hist_cat_data <- hist_data %>%
+full_hist_cat_date <- hist_data %>%
                     mutate(product_type = case_when(grepl('preprint', product) ~ 'preprints',
                                                     grepl('osf4m', product) ~ 'osf4m',
                                                     grepl('campaign', product) | grepl('regist', product) ~ 'registries',
                                                     grepl('osf', product) ~ 'osf')) %>%
                     group_by(product_type, month) %>%
                     summarize(new_signups = sum(new_signups), new_claims = sum(new_claims), new_invitees = sum(new_invitees), 
-                              sso_newsignups = sum(sso_newsignups), sso_newclaims = sum(sso_newclaims))
-
-
-
-full_hist_cat_date <- full_join(date, products, by = 'placeholder') %>%
-                          left_join(hist_cat_data, by = c("date" = "month", 'product_type' = 'product_type')) %>%
-                          select(-placeholder) %>%
-                          replace(., is.na(.), 0) %>%
-                          group_by(product_type) %>%
-                          mutate(cum_new_signups = cumsum(new_signups),  #create columns for running totals
-                                 cum_new_claims = cumsum(new_claims),
-                                 cum_new_sources = cumsum(new_sources),
-                                 cum_sso_newsignups = cumsum(sso_newsignups),
-                                 cum_sso_newclaims = cumsum(sso_newclaims))
+                              sso_newsignups = sum(sso_newsignups), sso_newclaims = sum(sso_newclaims)) %>%
+                    arrange(month) %>%
+                    group_by(product_type) %>%
+                    mutate(cum_new_signups = cumsum(new_signups),  #create columns for running totals
+                           cum_new_claims = cumsum(new_claims),
+                           cum_new_sources = cumsum(new_invitees),
+                           cum_sso_newsignups = cumsum(sso_newsignups),
+                           cum_sso_newclaims = cumsum(sso_newclaims))
 
 # write out full_hist_cat_date for use in dashboard
 write_csv(full_hist_cat_date, 'full_hist_cat_date.csv')

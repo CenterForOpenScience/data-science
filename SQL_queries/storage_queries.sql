307c2d9c64df3f34c902734e2a4b5114f1f0fb4e
csoderberg <csoderbe@gmail.com>
2020-09-01 10:35:53 -0400

    add handling of nulls

--- SQL_queries/storage_queries.sql
+++ SQL_queries/storage_queries.sql
@@ -93,7 +93,7 @@ SELECT COUNT(DISTINCT root_id) AS num_toplevel_reg,
 	ON registries_files.file_id = actions_file.file_id
 	
 
-/* number of different object types and storage by 3 example OSF4I */
+/* number of different object types and storage by each non-COS OSF4I */
 WITH osf4I_files AS (SELECT root_id,
 							nodes.type,
 							nodes.id AS node_id,
@@ -110,9 +110,7 @@ WITH osf4I_files AS (SELECT root_id,
 									 FROM osf_basefilenode
 									 WHERE type = 'osf.osfstoragefile') as osf_files
 						ON nodes.id = osf_files.target_object_id
-						WHERE osf_abstractnode_affiliated_institutions.institution_id = 17 OR 
-						osf_abstractnode_affiliated_institutions.institution_id = 58 OR 
-						osf_abstractnode_affiliated_institutions.institution_id = 52),
+						WHERE osf_abstractnode_affiliated_institutions.institution_id != 65),
 	 file_actions AS (SELECT file_id, Min(_id), action, Max(total),
 	 						 CASE WHEN action = 'download' THEN Max(total) ELSE 0 END AS downloads,
 							 CASE WHEN action = 'view' THEN Max(total) ELSE 0 END AS views
@@ -121,17 +119,17 @@ WITH osf4I_files AS (SELECT root_id,
 						GROUP BY file_id, action)
 
 SELECT name,
-		COUNT(DISTINCT (CASE WHEN type = 'osf.node' THEN root_id END)) AS num_topleve_projects,
-		COUNT(DISTINCT (CASE WHEN type = 'osf.registration' THEN root_id END)) AS num_topleve_reg,
-		COUNT(DISTINCT (CASE WHEN type = 'osf.node' THEN node_id END)) AS num_nonreg_nodes,
-		COUNT(DISTINCT (CASE WHEN type = 'osf.registration' THEN node_id END)) AS num_reg_nodes,
-		COUNT(DISTINCT (CASE WHEN type = 'osf.node' THEN osf4I_files.file_id END)) AS num_nonreg_files,
-		COUNT(DISTINCT (CASE WHEN type = 'osf.registration' THEN osf4I_files.file_id END)) AS num_reg_files,
-		SUM(CASE WHEN type = 'osf.node' THEN num_versions END) AS num_nonreg_file_version,
-		SUM(CASE WHEN type = 'osf.registration' THEN num_versions END) AS num_reg_file_versions,
-		SUM(storage) AS storage,
-		SUM(downloads) AS downloads,
-	    SUM(views) AS views
+		COALESCE(COUNT(DISTINCT (CASE WHEN type = 'osf.node' THEN root_id END)),0) AS num_topleve_projects,
+		COALESCE(COUNT(DISTINCT (CASE WHEN type = 'osf.registration' THEN root_id END)),0) AS num_topleve_reg,
+		COALESCE(COUNT(DISTINCT (CASE WHEN type = 'osf.node' THEN node_id END)) AS num_nonreg_nodes,
+		COALESCE(COUNT(DISTINCT (CASE WHEN type = 'osf.registration' THEN node_id END)),0) AS num_reg_nodes,
+		COALESCE(COUNT(DISTINCT (CASE WHEN type = 'osf.node' THEN osf4I_files.file_id END)),0) AS num_nonreg_files,
+		COALESCE(COUNT(DISTINCT (CASE WHEN type = 'osf.registration' THEN osf4I_files.file_id END)),0) AS num_reg_files,
+		COALESCE(SUM(CASE WHEN type = 'osf.node' THEN num_versions END),0) AS num_nonreg_file_version,
+		COALESCE(SUM(CASE WHEN type = 'osf.registration' THEN num_versions END),0) AS num_reg_file_versions,
+		COALESCE(SUM(storage),0) AS storage,
+		COALESCE(SUM(downloads),0) AS downloads,
+	    COALESCE(SUM(views),0) AS views
 	FROM osf4I_files
 	LEFT JOIN (SELECT osf_basefilenode.id, Max(target_object_id) as target_object_id, SUM(size) AS storage, COUNT(DISTINCT osf_fileversion.id) AS num_versions
 					FROM osf_basefilenode
